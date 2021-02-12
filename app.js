187fe6efc8bee2e1d175158734f315e8c4ccfbad
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-02-12 16:22:01 -0600

    added new html templates to page-template.js

--- app.js
+++ app.js
@@ -131,14 +131,14 @@ const promptProject = portfolioData => {
     });
 };
 promptUser().then(promptProject).then(portfolioData => {
-    console.log(portfolioData);
+  const pageHTML = generatePage(portfolioData);
+ fs.writeFile('./index.html', pageHTML, err => {
+   if (err) throw err;
+      console.log('Portfolio complete! Check out index.html to see the output!');
+  });
 });
 
 
 
-//  const pageHTML = generatePage(name, github);
-//  fs.writeFile('./index.html', pageHTML, err => {
-//      if (err) throw err;
-//      console.log('Portfolio complete! Check out index.html to see the output!');
-//  });
+
 
