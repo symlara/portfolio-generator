9d43bee485af7a7e5e55ffc8bdc7e2126d668b3c
Aaliyah <aaliyah.symlar@vanderbilt.edu>
2021-02-11 21:54:07 -0600

    added fs code back to top of app.js file

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
+const fs = require('fs');
 const inquirer = require('inquirer');
+const generatePage = require('./src/page-template');
 
 const promptUser = () => {
 return inquirer.prompt([
@@ -132,14 +134,11 @@ promptUser().then(promptProject).then(portfolioData => {
     console.log(portfolioData);
 });
 
-// const fs = require('fs');
-// const generatePage = require('./src/page-template');
 
-// const pageHTML = generatePage(name, github);
 
-// fs.writeFile('./index.html', pageHTML, err => {
-//     if (err) throw err;
-
-//     console.log('Portfolio complete! Check out index.html to see the output!');
-// });
+//  const pageHTML = generatePage(name, github);
+//  fs.writeFile('./index.html', pageHTML, err => {
+//      if (err) throw err;
+//      console.log('Portfolio complete! Check out index.html to see the output!');
+//  });
 
